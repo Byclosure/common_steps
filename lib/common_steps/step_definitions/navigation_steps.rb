acc8e383aedd8bac7729985bf7d6d49f53fe1939
Vasco <vasco@byclosure.com>
2009-09-08 18:38:17 +0100

    base, not worked yet

---
+++ lib/common_steps/step_definitions/navigation_steps.rb
@@ -0,0 +1,52 @@
+When /^I (\w+) the form with$/ do |submit_name, table|
+  pending
+end
+
+When /^I go to "([^\"]*)"$/ do |page_name|
+  visit page_name
+end
+
+When /^I go to the homepage$/ do
+  When 'I go to "/"'
+end
+
+Given /^I am on the new (\w+) page$/ do |record_name|
+  When "I go to the new #{record_name} page"
+end
+
+When /^I go to the new (\w+) page$/ do |record_name|
+  record_class = record_name_to_class(record_name)
+  When "I go to \"/#{record_class_to_path(record_class)}/new\""
+end
+
+When /^I go to the list page of (\w+)$/ do |record_name|
+  path = record_name.pluralize # TODO refact
+  When "I go to \"/#{path}\""
+end
+
+When /^I go to the edit page of (\w+) with (.*)$/ do |record_name, record_conditions|
+  record_class = record_name_to_class(record_name)
+  record = find_record(record_class, record_conditions)
+  When "I go to \"/#{record_class_to_path(record_class)}/#{record.id}/edit\""
+end
+
+When /^I go to the show page of (\w+) with (.*)$/ do |record_name|
+  record_class = record_name_to_class(record_name)
+  record = record_find(record_class, record_conditions)
+  When "I go to \"/#{record_class_to_path(record_class)}/#{record.id}\""
+end
+
+#When /^I go to the delete page of (\w+) with (.*)$/ do |record_name|
+#  pending
+#end
+
+Then /^I should be on "([^\"]*)"$/ do |page_name|
+  URI.parse(current_url).path.should == page_name
+end
+
+Then /^I should be on the show page of (\w+) with (.*)$/ do |record_name, record_conditions|
+  record_class = record_name_to_class(record_name)
+  record = find_record(record_class, record_conditions)
+  path = "/#{record_class_to_path(record_class)}/#{record.id}"
+  URI.parse(current_url).path.should match(/#{path}/)
+end

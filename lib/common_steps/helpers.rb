06447251dc91764dae502c44fe88a32cf56b17de
dsgh <duarte.henriques@gmail.com>
2009-09-09 14:26:16 +0100

    automatically include necessary definitions

--- lib/common_steps/helpers.rb
+++ lib/common_steps/helpers.rb
@@ -1,7 +1,11 @@
+common_step_mods = []
+
 Dir[File.dirname(__FILE__) + "/helpers/*.rb"].each do |filename|
   require filename
   module_name = filename.chomp(".rb").classify.demodulize
   mod = module_name.constantize rescue warn("unable to load: module `#{module_name}' in file #{filename}")
-  World(mod) if defined?(World)
+  common_step_mods << mod
 end
+
+Cucumber::RbSupport::RbDsl.build_rb_world_factory(common_step_mods, nil)
 

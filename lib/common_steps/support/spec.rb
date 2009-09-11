0e9b655c43804456d2e54165e96deb1b213ef930
dsgh <duarte.henriques@gmail.com>
2009-09-11 23:12:54 +0100

    fix matcher requires

--- lib/common_steps/support/spec.rb
+++ lib/common_steps/support/spec.rb
@@ -1,11 +1,3 @@
-require 'spec/autorun'
-require 'spec/rails'
-require 'spec/mocks'
-
-Dir[File.expand_path(File.dirname(__FILE__) + "../matchers/*.rb")].each do |matcher_fn|
-  require matcher_fn
+Dir[File.expand_path(File.dirname(__FILE__) + "/../matchers/*.rb")].each do |filename|
+  require filename
 end
-
-# Spec::Runner.configure do |config|
-# end
-

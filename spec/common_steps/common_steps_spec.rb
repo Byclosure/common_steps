000102026965673493145e09f9936361a936f62a
LuisJoao <luis@byclosure.com>
2009-09-11 02:49:17 +0200

    specs refactored

--- spec/common_steps/common_steps_spec.rb
+++ spec/common_steps/common_steps_spec.rb
@@ -2,12 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../common_steps_helper')
 require "common_steps/helpers/record_helper"
 
+module StepMotherHelper
+  def step_should_match(step_string)
+    it { @step_mother.should step_match(step_string) }  
+  end
+  #  def step_should_not_match(step_string)
+  #    it { @step_mother.should_not step_match(step_string) }  
+  #  end
+  
+  def artist_should_count(n)
+    is_or_are = n == 1 ? "is" : "are"
+    plural_or_singular = n == 1 ? "" : "s" 
+    it "should count #{n} Artist#{plural_or_singular} when I call 'there #{is_or_are} #{n} artist#{plural_or_singular}'" do
+      m = @step_mother.step_match("there #{is_or_are} #{n} artist#{plural_or_singular}")
+      m.invoke(nil)
+      Artist.should count(n)
+    end
+  end
+end
+
 # In order to use features without bugs
 # As a developer
 # I want to test my applications using record steps beginning with Given
 describe "Cucumber instance with record_steps.rb and World(RecordHelper) loaded" do
+  extend StepMotherHelper
+  
   before do
-    #record_steps.rb is loaded
+    @empty_table = Cucumber::Ast::Table.new([])
     @cucumber_table = Cucumber::Ast::Table.new(
       [['name', 'age'],
         ['John', '12'],
@@ -20,10 +41,23 @@ describe "Cucumber instance with record_steps.rb and World(RecordHelper) loaded"
     @step_mother.load_programming_language('rb').begin_rb_scenario
   end
   
+  step_should_match 'the following artists'
+  step_should_match 'there is a artist'
+  step_should_match 'there is an artist'
+  step_should_match 'there is 0 artists'
+  step_should_match 'there is 1 artist'
+  step_should_match 'there are 3 artists'
   
-  it "string 'Given the following artists' should match Step[Given the following (\w+)]" do
-    @step_mother.step_match('the following artists')
+  describe "StepMatch[there (is|are) (\w+) (.*)] invokation, when there are no Artists" do
+    before do
+      Artist.destroy_all # there are no artist - FIXME I shouldn't have to care about this
+    end
+    
+    artist_should_count(0)
+    artist_should_count(1)
+    artist_should_count(3)    
   end
+  
   
   describe "'Given the following artists' invoked with table of attributes" do
     it "should create the records" do
@@ -35,14 +69,4 @@ describe "Cucumber instance with record_steps.rb and World(RecordHelper) loaded"
     end
   end
 
-  it "string 'Given there (is|are) (\w+) (\w+) with a (.*)' should match Step[Given the following (\w+)]" do
-    @step_mother.step_match('the following artists')
-  end
-  
-  
-  
-  it "calling 'Given the following artists' should be true" do
-    
-  end
-  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,49 @@ require 'spec/mocks'
 
 Spec::Runner.configure do |config|
 end
+
+
+Spec::Matchers.define :step_match do |expected_string|
+  match do |step_mother|
+    begin
+      step_mother.step_match(expected_string)
+    rescue Exception => @exception
+      @exception
+    end
+    @exception.nil?
+  end
+  
+  failure_message_for_should do |step_mother|
+    "expected #{step_mother} to StepMatch[#{expected_string}], exception raised:\n" + 
+      "Type: #{@exception.class}\n" +
+      "Message: #{@exception.message}"
+  end
+  
+  failure_message_for_should_not do |step_mother|
+    # "expected #{collection} count not to be #{num}"
+    "TODO"
+  end
+
+  description do
+    "StepMatch[#{expected_string}]"
+  end
+end
+
+#Copied from count.rb
+Spec::Matchers.define :count do |num|
+  match do |collection|
+    collection.count == num
+  end
+  
+  failure_message_for_should do |collection|
+    "expected #{collection} count to be #{num} instead of #{collection.count}"
+  end
+  
+  failure_message_for_should_not do |collection|
+    "expected #{collection} count not to be #{num}"
+  end
+
+  description do
+    "count should be #{num}"
+  end
+end

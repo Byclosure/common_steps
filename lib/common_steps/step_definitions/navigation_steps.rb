257fb0024430d93059a0a9d6d226975c8b4aaad7
LuisJoao <luis@byclosure.com>
2009-09-15 03:07:51 +0200

    adapt some steps

--- lib/common_steps/step_definitions/navigation_steps.rb
+++ lib/common_steps/step_definitions/navigation_steps.rb
@@ -31,13 +31,13 @@ end
 When /^I go to the edit page of the (\w+) with (a|an) (.*)$/ do |record_name, _, record_conditions|
   record_class = record_name_to_class(record_name)
   record = find_record(record_class, record_conditions)
-  When "I go to \"/#{record_class_to_path(record_class)}/#{record.id}/edit\""
+  When "I go to \"#{record_class_to_path(record_class)}/#{record.id}/edit\""
 end
 
 When /^I go to the show page of the (\w+) with (a|an) (.*)$/ do |record_name, _, record_conditions|
   record_class = record_name_to_class(record_name)
   record = find_record(record_class, record_conditions)
-  When "I go to \"/#{record_class_to_path(record_class)}/#{record.id}\""
+  When "I go to \"#{record_class_to_path(record_class)}/#{record.id}\""
 end
 
 #When /^I go to the delete page of (\w+) with (.*)$/ do |record_name|
@@ -51,16 +51,23 @@ end
 Then /^I should be on the show page of the (\w+) with (a|an) (.*)$/ do |record_name, _,record_conditions|
   record_class = record_name_to_class(record_name)
   record = find_record(record_class, record_conditions)
-  path = "/#{record_class_to_path(record_class)}/#{record.id}"
+  path = "#{record_class_to_path(record_class)}/#{record.id}"
   URI.parse(current_url).path.should match(/#{path}/)
 end
 
 Then /^I should be on the edit page of the (\w+) with (a|an) (.*)$/ do |record_name, _,record_conditions|
   record_class = record_name_to_class(record_name)
   record = find_record(record_class, record_conditions)
-  path = "/#{record_class_to_path(record_class)}/#{record.id}"
+  path = "#{record_class_to_path(record_class)}/#{record.id}"
   URI.parse(current_url).path.should match(/#{path}/)
 end
+
+Then /^I should be on the "(\w+)" create page$/ do |record_name|
+  record_class = record_name_to_class(record_name)
+  path = "#{record_class_to_path(record_class)}/new"
+  URI.parse(current_url).path.should match(/#{path}/)
+end
+
 
 Then /the field with label "([^\"]*)" should contain "([^\"]*)"/ do |field_name, content|
   field_labeled(field_name).value.should == content

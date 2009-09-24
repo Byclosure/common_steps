969521776cec0bdb1e4ae2e1398c0e5ce494ed11
Vasco <vasco@byclosure.com>
2009-09-24 20:20:35 +0100

    email supported in all transactions

--- lib/common_steps/support/email.rb
+++ lib/common_steps/support/email.rb
@@ -2,13 +2,13 @@ require "rumbster"
 require "message_observers" # same gem as rumbster
 
 rumbster = nil
-Before('@email') do
+Before do
   rumbster = Rumbster.new(ActionMailer::Base.smtp_settings[:port])
   @email_inbox = MailMessageObserver.new
   rumbster.add_observer @email_inbox
   rumbster.start
 end
 
-After('@email') do
+After do
   rumbster.stop
 end

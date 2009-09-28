bbb86f9731cf7ba88e308b226bc9f9e16f506242
dsgh <duarte.henriques@gmail.com>
2009-09-28 12:14:45 +0100

    allow running with custom step_mother

--- lib/common_steps.rb
+++ lib/common_steps.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.dirname(__FILE__) + "/common_steps/helpers")
-Cucumber::Cli::Main.step_mother.load_code_files(Dir[File.expand_path(File.dirname(__FILE__) + "/common_steps/step_definitions/**/*.*")])
+step_mother = $step_mother || Cucumber::Cli::Main.step_mother
+step_mother.load_code_files(Dir[File.expand_path(File.dirname(__FILE__) + "/common_steps/step_definitions/**/*.*")])
